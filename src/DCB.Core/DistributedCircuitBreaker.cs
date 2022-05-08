5d46f1a193b76b9a00dbbfc32cc4a8f440cb1880
Shamil Sadigov <sov.shamil@gmail.com>
2022-05-08 21:56:16 +0400

    Add initial classes

---
+++ src/DCB.Core/DistributedCircuitBreaker.cs
@@ -0,0 +1,15 @@
+﻿using Polly;
+
+namespace DCB.Core;
+
+public class DistributedCircuitBreaker:AsyncPolicy
+{
+    protected override Task<TResult> ImplementationAsync<TResult>(
+        Func<Context, CancellationToken, Task<TResult>> action,
+        Context context,
+        CancellationToken cancellationToken,
+        bool continueOnCapturedContext)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/DCB.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DCB.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDistributedCircuitBreaker(
+        this IServiceCollection services)
+    {
+        // configure here
+        
+        return services;
+    }
+}
