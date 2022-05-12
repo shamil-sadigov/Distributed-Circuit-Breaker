f1b8d266ee97e05f54faebd570f4cc7af6e7fc34
Shamil Sadigov <sov.shamil@gmail.com>
2022-05-12 19:36:41 +0400

    Add overload to ExceptionHandlers

--- src/DCB.Core/Handlers/ExceptionHandlers/ExceptionHandlers.cs
+++ src/DCB.Core/Handlers/ExceptionHandlers/ExceptionHandlers.cs
@@ -5,11 +5,11 @@ namespace DCB.Core.Handlers.ExceptionHandlers;
 /// <summary>
 /// TODO: Write unit tests
 /// </summary>
-internal sealed class ExceptionHandlers
+public sealed class ExceptionHandlers
 {
     private readonly HashSet<IExceptionHandler> _exceptionHandlers = new();
     
-    internal ExceptionHandlers Handle(IExceptionHandler exceptionHandler)
+    public ExceptionHandlers Handle(IExceptionHandler exceptionHandler)
     {
         exceptionHandler.ThrowIfNull();
         _exceptionHandlers.Add(exceptionHandler);
@@ -17,7 +17,15 @@ internal sealed class ExceptionHandlers
         return this;
     }
 
-    internal ExceptionHandlers Handle<TException>(Func<TException, bool> exceptionHandler) where TException : Exception
+    public ExceptionHandlers Handle<TException>() 
+        where TException : Exception
+    {
+        var handler = new DelegateBasedExceptionHandler(ex => ex is TException);
+        _exceptionHandlers.Add(handler);
+        return this;
+    }
+    
+    public ExceptionHandlers Handle<TException>(Func<TException, bool> exceptionHandler) where TException : Exception
     {
         exceptionHandler.ThrowIfNull();
         var handler = new DelegateBasedExceptionHandler(ex => ex is TException typedEx && exceptionHandler(typedEx));
