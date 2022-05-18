811e48fa411bcd53ad40bc636afd54599405cddb
Shamil Sadigov <sov.shamil@gmail.com>
2022-05-19 00:50:27 +0400

    Add MongoStorage integration tests

---
+++ src/DCP.Extensions.Mongo.Tests/Helpers/MongoOptionsProvider.cs
@@ -0,0 +1,39 @@
+﻿using DCB.Extensions.Mongo;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace DCP.Extensions.Mongo.Tests.Helpers;
+
+public sealed class MongoOptionsProvider : IAsyncLifetime
+{
+    private const string ConfigFileName = "test-config.json";
+
+    public MongoClient MongoClient { get; private set; } = null!;
+    public CircuitBreakerDbOptions Options { get; set; } = null!;
+    
+    public Task InitializeAsync()
+    {
+        var connectionString = GetConnectionStringFromConfiguration();
+        MongoClient = new MongoClient(connectionString);
+        Options = new CircuitBreakerDbOptions("DistributedCircuitBreakerTestDB", "CircuitBreakers");
+        return Task.CompletedTask;
+    }
+    
+    public async Task DisposeAsync() 
+        => await MongoClient.DropDatabaseAsync(Options.DatabaseName);
+    
+    private static string GetConnectionStringFromConfiguration()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile(ConfigFileName)
+            .Build();
+
+        var connectionString = config.GetConnectionString("Default");
+
+        if (connectionString is null)
+            throw new InvalidOperationException(
+                $"Connection string is not provided in configuration file '{ConfigFileName}'");
+
+        return connectionString;
+    }
+}
---
+++ src/DCP.Extensions.Mongo.Tests/MappingTests.cs
@@ -0,0 +1,54 @@
+using AutoMapper;
+using DCB.Core.CircuitBreakers.Context;
+using DCB.Extensions.Mongo;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+
+namespace DCP.Extensions.Mongo.Tests;
+
+public class MappingTests
+{
+    private readonly Mapper _mapper;
+
+    public MappingTests()
+    {
+        _mapper = new Mapper(new MapperConfiguration(x=> x.AddMaps(typeof(DataModelProfile))));
+    }
+    
+    [Fact]
+    public void Snapshot_is_correctly_mapped_to_data_model()
+    {
+        var snapshot = new CircuitBreakerContextSnapshot
+        (
+            Name: "CircuitBreakerName",
+            FailureAllowedBeforeBreaking: 5,
+            FailedCount: 5,
+            IsCircuitBreakerClosed: true,
+            TransitionDateToHalfOpenState: DateTime.UtcNow + 10.Seconds(),
+            LastTimeStateChanged: DateTime.UtcNow - 10.Seconds(),
+            DurationOfBreak: 20.Seconds()
+        );
+        
+        var dataModel = _mapper.Map<CircuitBreakerDataModel>(snapshot);
+
+        dataModel.Should().BeEquivalentTo(snapshot);
+    }
+    
+    [Fact]
+    public void Datamodel_is_correctly_mapped_to_snapshot()
+    {
+        var dataModel = new CircuitBreakerDataModel
+        {
+            Name = "CircuitBreakerName",
+            FailureAllowedBeforeBreaking = 5,
+            FailedCount = 5,
+            TransitionDateToHalfOpenState = DateTime.UtcNow + 10.Seconds(),
+            LastTimeStateChanged = DateTime.UtcNow - 10.Seconds(),
+            DurationOfBreak = 20.Seconds()
+        };
+            
+        var snapshot = _mapper.Map<CircuitBreakerContextSnapshot>(dataModel);
+
+        snapshot.Should().BeEquivalentTo(dataModel);
+    }
+}
