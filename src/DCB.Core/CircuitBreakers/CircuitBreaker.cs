8ff40705b327dbe28cb3c77fa1ccee729671218b
Shamil Sadigov <sov.shamil@gmail.com>
2022-05-19 17:16:04 +0400

    Refactor CircuitBreaker and extract an interface

--- src/DCB.Core/CircuitBreakers/CircuitBreaker.cs
+++ src/DCB.Core/CircuitBreakers/CircuitBreaker.cs
@@ -1,63 +1,72 @@
 ﻿using DCB.Core.CircuitBreakerOption;
 using DCB.Core.CircuitBreakers.Context;
+using DCB.Core.Exceptions;
 using DCB.Core.Storage;
 
 namespace DCB.Core.CircuitBreakers;
 
+public class CircuitBreaker<TOptions> : ICircuitBreaker<TOptions> where TOptions : CircuitBreakerOptions
 {
-
-internal struct Void
-{
-}
-
-public class CircuitBreaker<TOptions> where TOptions : CircuitBreakerOptions
-{
-    private readonly ICircuitBreakerContextGetter _contextGetter;
+    private readonly ICircuitBreakerStorage _storage;
     private readonly CircuitBreakerStateHandlerProvider _handlerProvider;
     private readonly ISystemClock _systemClock;
-    public readonly TOptions CircuitBreakerOptions;
-
-    // TODO: Add context property
-
-    protected CircuitBreaker(
-        ICircuitBreakerContextGetter contextGetter,
+    private readonly TOptions _circuitBreakerOptions;
+    
+    public CircuitBreaker(
+        ICircuitBreakerStorage storage,
         CircuitBreakerStateHandlerProvider handlerProvider,
         TOptions circuitBreakerOptions,
         ISystemClock systemClock)
     {
-        _contextGetter = contextGetter;
+        _storage = storage;
         _handlerProvider = handlerProvider;
-        CircuitBreakerOptions = circuitBreakerOptions;
+        _circuitBreakerOptions = circuitBreakerOptions;
         _systemClock = systemClock;
+    }
+
+    public async Task<CircuitBreakerState> GetStateAsync()
+    {
+        var snapshot = await _storage.GetAsync(_circuitBreakerOptions.Name, CancellationToken.None);
+        
+        if (snapshot is null)
+            throw new CircuitBreakerSnapshotNotFoundException(_circuitBreakerOptions.Name);
+
+        var circuitBreakerContext = CircuitBreakerContext.BuildFromSnapshot(snapshot, _systemClock.GetCurrentTime());
+
+        return circuitBreakerContext.State;
     }
 
     public async Task<TResult> ExecuteAsync<TResult>(
         Func<CancellationToken, Task<TResult>> action,
         CancellationToken cancellationToken)
     {
-        var snapshot = await _contextGetter
-            .GetAsync(CircuitBreakerOptions.Name, cancellationToken)
+        var snapshot = await _storage
+            .GetAsync(_circuitBreakerOptions.Name, cancellationToken)
             .ConfigureAwait(false);
-
-        var context = BuildOrCreateCircuitBreaker<TResult>(snapshot);
+        
+        CircuitBreakerContext context;
+        
+        if (snapshot is not null)
+            context = CircuitBreakerContext.BuildFromSnapshot(snapshot, _systemClock.GetCurrentTime());
+        else
+            context = await CreateNewCircuitBreakerAsync(cancellationToken);
 
         var stateHandler = _handlerProvider.GetHandler(context);
 
-        var result = await stateHandler.HandleAsync(action, context, CircuitBreakerOptions, cancellationToken);
+        var result = await stateHandler.HandleAsync(action, context, _circuitBreakerOptions, cancellationToken);
 
         return result;
     }
 
-    private CircuitBreakerContext BuildOrCreateCircuitBreaker<TResult>(CircuitBreakerContextSnapshot? snapshot)
+    private async Task<CircuitBreakerContext> CreateNewCircuitBreakerAsync(CancellationToken token)
     {
-        CircuitBreakerContext context;
-        if (snapshot is not null)
-            context = CircuitBreakerContext.BuildFromSnapshot(snapshot, _systemClock.CurrentTime);
-        else
-            context = CircuitBreakerContext.CreateNew(
-                CircuitBreakerOptions.Name,
-                CircuitBreakerOptions.FailureAllowedBeforeBreaking,
-                CircuitBreakerOptions.DurationOfBreak);
+        var context = CircuitBreakerContext.CreateNew(
+                _circuitBreakerOptions.Name,
+                _circuitBreakerOptions.FailureAllowedBeforeBreaking,
+                _circuitBreakerOptions.DurationOfBreak);
+
+        var snapshot = context.GetSnapshot();
+        await _storage.AddAsync(snapshot, token);
         return context;
     }
 }
---
+++ src/DCB.Core/CircuitBreakers/ICircuitBreaker.cs
@@ -0,0 +1,12 @@
+﻿using DCB.Core.CircuitBreakerOption;
+
+namespace DCB.Core.CircuitBreakers;
+
+public interface ICircuitBreaker<TOptions> where TOptions : CircuitBreakerOptionsBase
+{
+    Task<CircuitBreakerState> GetStateAsync();
+    
+    Task<TResult> ExecuteAsync<TResult>(
+        Func<CancellationToken, Task<TResult>> action,
+        CancellationToken cancellationToken);
+}
