cb9c052c2b31cbee5a79d435da04e0d63df0e1e3
Shamil Sadigov <sov.shamil@gmail.com>
2022-05-28 16:15:18 +0400

    Reorder fields

--- tests/DCB.Core.Tests/CircuitBreakerTests/Helpers/CurrencyExchangeResponse.cs
+++ tests/DCB.Core.Tests/CircuitBreakerTests/Helpers/CurrencyExchangeResponse.cs
@@ -3,20 +3,18 @@
 public class CurrencyExchangeResponse
 {
     public bool IsExchangeAvailableForToday { get; set; }
-    
-    // image other properties...
 
-    public static readonly CurrencyExchangeResponse Successful = new()
+    private static readonly CurrencyExchangeResponse Successful = new()
     {
         IsExchangeAvailableForToday = true
     };
-    
-    public static readonly Task<CurrencyExchangeResponse> SuccessfulResult = Task.FromResult(Successful);
-    
-    public static readonly CurrencyExchangeResponse Unsuccessful = new()
+
+    private static readonly CurrencyExchangeResponse Unsuccessful = new()
     {
         IsExchangeAvailableForToday = false
     };
+    
+    public static readonly Task<CurrencyExchangeResponse> SuccessfulResult = Task.FromResult(Successful);
     
     public static readonly Task<CurrencyExchangeResponse> UnsuccessfulResult = Task.FromResult(Unsuccessful);
 
