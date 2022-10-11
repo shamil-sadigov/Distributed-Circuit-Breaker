96ade4e25875f8b08ed335d9f4568ddd99370195
Shamil Sadiqov <sov.shamil@gmail.com>
2022-10-12 01:07:35 +0400

    Remove SQL Server storage

--- src/Storage.SqlServer/README.md
+++
@@ -1,2 +0,0 @@
-
-Currently, EF is used for connecting with SQL Server, but will be replaced with ADO.NET or Dapper
--- src/Storage.SqlServer/core/CircuitBreakerDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Storage.SqlServer;
-
-public class CircuitBreakerDataModel
-{
-    public string Name { get; set; }
-    public int FailedCount { get; set; }
-    public DateTime? LastTimeStateChanged { get; set; }
-    public TimeSpan DurationOfBreak { get; set; }
-}
