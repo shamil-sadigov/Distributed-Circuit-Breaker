fd900a1ec8316894bbcde8a86aa6a0283fc3b360
Shamil Sadiqov <sov.shamil@gmail.com>
2022-10-11 21:54:04 +0400

    Add comments to CircuitBreakerSettings

--- src/Core/src/Settings/CircuitBreakerSettings.cs
+++ src/Core/src/Settings/CircuitBreakerSettings.cs
@@ -5,9 +5,21 @@ namespace Core.Settings;
 
 public abstract partial class CircuitBreakerSettings
 {
+    /// <summary>
+    /// Unique name of circuit breaker
+    /// </summary>
+    // TODO: Ensure no longer than 256 char
     public abstract string Name { get; }
-    public abstract int FailureAllowedBeforeBreaking { get; }
-    public abstract TimeSpan DurationOfBreak { get; }
+    
+    /// <summary>
+    /// Number of failures allowed before breaking circuit breaker (e.g moving circuit breaker in Open state).
+    /// </summary>
+    public abstract int FailureAllowed { get; set; }
+    
+    /// <summary>
+    /// How long does circuit breaker is allowed to be in Open state
+    /// </summary>
+    public abstract TimeSpan DurationOfBreak { get; set; }
     
     private ExceptionHandlers ExceptionHandlers { get; } = new();
     private ResultHandlers ResultHandlers { get; } = new();
